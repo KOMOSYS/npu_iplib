5faf2fb142cafcd403abca6d0edbc05075a49dfa
kjs <kjs@deepx.ai>
2024-03-08 10:58:02 +0900

    lint update

--- script/lint/run.py
+++ script/lint/run.py
@@ -8,6 +8,60 @@ LM_LICENSE_FILE = "27020@deepx02:27020@deepx03"
 SPYGLASS_HOME = "/tools/synopsys/spyglass/U-2023.03/SPYGLASS_HOME/"
 GATE_LIB = "/DKIT/ARM_5LPE/STD_CELL/ln05lpe/LIB/sc6mcz_ln05lpe_base_rvt_c10_sspg_nominal_max_0p675v_m40c.lib"
 
+def set_bbox():
+    set_bbox = ""
+    bbox_list = [
+    "rf_sp_hse_640x96m2b2s2w0p0d1v0t0r1200c1a1", 
+    "rf_sp_hse_128x96m2b1s2w0p0d1v0t0r1200c1a1", 
+    "rf_sp_hse_1024x128m2b2s2w0p0d1v1t0r1200c1a1", 
+    "clk_buf_6t", 
+    "clk_gate_6t"]
+    for bbox in bbox_list:
+        set_bbox += f"set_option stop {bbox}\n"
+    return set_bbox
+
+def set_parameter():
+    set_parameter = ""
+    parameter_list = ["checkfullbus", "checkfullrecord", "handle_large_bus"]
+    for parameter in parameter_list:
+        set_parameter += f"set_parameter {parameter} yes\n"
+    return set_parameter
+
+def change_goal_severity():
+    change_goal_severity = ""
+    Info_goal_list = ["W111", "W240", "w498", "W287b"]
+    for goal in Info_goal_list:
+        change_goal_severity += f"set_goal_option overloadrules {goal}+severity=Info\n"
+
+    warn_goal_list = ["STARC-2.3.4.3"]
+    for goal in warn_goal_list:
+        change_goal_severity += f"set_goal_option overloadrules {goal}+severity=Warning\n"
+
+    error_goal_list = ["W164c", "W164a", "W224", "W287a", "W287c", "W339a"]
+    for goal in error_goal_list:
+        change_goal_severity += f"set_goal_option overloadrules {goal}+severity=Error\n"
+    
+    return change_goal_severity
+
+def set_goal():
+    set_goal = ""
+    goal_list = [
+    "W18", "W123", "W164a", "W164b", "W164c", "W287a", "W336", "W415", "W339a",
+    "W111", "W488", "W69", "W71", "W263", "W392", "W448", "W391", "W401", "W120", 
+    "W121", "W494a", "W494b", "W497", "W498", "W541", "W19", "W414", "W484", "W110", 
+    "W156", "W116", "W362", "W486", "W415", "W552", "W553", "W352", "W479", "W480", 
+    "W481a", "W481b", "W287b"]
+    for goal in goal_list:
+        set_goal += f"set_goal_option addrule {goal}\n"
+    return set_goal
+
+def waive_module():
+    waive_module = ""
+    for module in ["DW01_cmp2", "DW_fp_flt2i"]:
+        waive_module += f"waive -msg {{Design Unit '{module}'   has empty definition}}  -rule {{  {{WarnAnalyzeBBox}}  }}\n"
+    return waive_module
+
+
 def set_env():
     os.environ["LM_LICENSE_FILE"] = "27020@deepx02:27020@deepx03"
     os.environ["PATH"] = f'{os.getenv("PATH")}:{SPYGLASS_HOME}/bin'
@@ -33,8 +87,12 @@ if __name__ == "__main__":
     run_tcl = run_tcl_path.read_text().format(
         top_name=args.top,
         file_list="./file_list_lint.f",
-        gate_lib=GATE_LIB,
-        spyglass_home=SPYGLASS_HOME
+        spyglass_home=SPYGLASS_HOME,
+        waive_module=waive_module(),
+        set_goal=set_goal(),
+        change_goal_severity=change_goal_severity(),
+        set_parameter=set_parameter(),
+        set_bbox=set_bbox()
     )
     Path("run.tcl").write_text(run_tcl)
     set_env()
--- script/lint/run.tcl
+++ script/lint/run.tcl
@@ -1,54 +1,41 @@
 #!/bin/tclsh -f
-set top_name {top_name}
-set file_list {file_list}
-set gate_lib {gate_lib}
-set spyglass_home {spyglass_home}
 
-new_project $top_name.lint -f
+#Create Session
+new_project {top_name}.lint -f
 
+#Design Setup
 set_option sort yes 
 set_option enableV05 yes
 set_option enableSV09 yes
+set_option inferblackbox yes
 set_option mthresh 9128448 
-set_option sgsyn_loop_limit 16384
 
-read_file -type sourcelist $file_list
-read_file -type gateslib $gate_lib
+read_file -type sourcelist {file_list}
 
-compile_design
-
-set_option top $top_name
+set_option top {top_name}
 set_option dw yes
+
+{waive_module}
+
+set_option sgsyn_loop_limit 16384
 set_option allow_module_override yes
 set_option define_cell_sim_depth 12
 set_option enable_gateslib_autocompile yes
-
-current_methodology $spyglass_home/GuideWare/latest/block/rtl_handoff
+#Goal Setup
+current_methodology {spyglass_home}/GuideWare/latest/block/rtl_handoff
 set_option auto_save yes
 
-current_goal lint/lint_rtl -top $top_name
-set_goal_option addrule       W18
-set_goal_option addrule       W123
-set_goal_option addrule       W164a
-set_goal_option addrule       W164b
-set_goal_option addrule       W164c
-set_goal_option addrule       W287a
-set_goal_option addrule       W336
-set_goal_option addrule       W415
-set_goal_option addrule       W339a
+current_goal lint/lint_rtl_enhanced     -top {top_name}
 
-set_goal_option overloadrules W164c+severity=Error
-set_goal_option overloadrules W164a+severity=Error
-set_goal_option overloadrules W224+severity=Error
-set_goal_option overloadrules W287a+severity=Error
-set_goal_option overloadrules W287b+severity=Error
-set_goal_option overloadrules W287c+severity=Error
-set_goal_option overloadrules W339a+severity=Error
+{set_goal}
 
-set_parameter checkfullbus    yes
-set_parameter checkfullrecord yes
-set_parameter handle_large_bus yes
+{change_goal_severity}
+
+{set_parameter}
+
+{set_bbox}
 
 run_goal
+
 save_project
 exit -force
